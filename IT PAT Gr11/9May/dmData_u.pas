e39f3d28098a33e1f95acb14fd259b7a88387c22
Tyrone <tvzbothma@gmail.com>
2024-05-09 19:27:42 +0200

    Add files via upload

---
+++ IT PAT Gr11/9May/dmData_u.pas
@@ -0,0 +1,30 @@
+unit dmData_u;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Data.DB, Data.Win.ADODB;
+
+type
+  TdmData = class(TDataModule)
+    ADOCon: TADOConnection;
+    tblUsers: TADOTable;
+    tblDiet: TADOTable;
+    dsUsers: TDataSource;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+    sUserID : string;
+  end;
+
+var
+  dmData: TdmData;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
