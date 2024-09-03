8766d19c4748f3bc24237dbae61e04838ffce10c
Tyrone <tvzbothma@gmail.com>
2024-09-03 11:34:57 +0200

    Add files via upload

---
+++ IT PAT Gr11/3Sep/frmSignUp_p.dpr
@@ -0,0 +1,24 @@
+program frmSignUp_p;
+
+uses
+  Vcl.Forms,
+  frmSignUp_u in 'frmSignUp_u.pas' {frmSignUp},
+  dmData_u in 'dmData_u.pas' {dmData: TDataModule},
+  frmHealthHarmony_u in 'frmHealthHarmony_u.pas' {frmHealth},
+  frmLogIn_u in 'frmLogIn_u.pas' {frmLogIn},
+  frmSettings_u in 'frmSettings_u.pas' {frmSettings},
+  frmWelcome_u in 'frmWelcome_u.pas' {frmWelcome};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfrmWelcome, frmWelcome);
+  Application.CreateForm(TfrmSignUp, frmSignUp);
+  Application.CreateForm(TdmData, dmData);
+  Application.CreateForm(TfrmHealth, frmHealth);
+  Application.CreateForm(TfrmLogIn, frmLogIn);
+  Application.CreateForm(TfrmSettings, frmSettings);
+  Application.Run;
+end.
